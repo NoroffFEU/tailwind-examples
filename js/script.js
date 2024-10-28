a3477c0788cbe957ebee2c7b02414b3d358cb54f
Connor O'Brien <connorbobrien@gmail.com>
2024-10-28 15:45:15 -0300

    Dark mode example

--- js/script.js
+++ js/script.js
@@ -1,6 +1,29 @@
-const btn = document.querySelector("#menu-btn");
-const menu = document.querySelector("#mobile-menu");
+function applyInitialTheme() {
+  const userPreference = localStorage.getItem("theme");
 
-btn.addEventListener("click", () => {
-  menu.classList.toggle("hidden");
-});
+  const systemPreferenceIsDark = window.matchMedia(
+    "(prefers-color-scheme: dark)"
+  ).matches;
+
+  const isDarkMode =
+    userPreference === "dark" || (!userPreference && systemPreferenceIsDark);
+
+  document.documentElement.classList.toggle("dark", isDarkMode);
+
+  document.querySelector("#sun").classList.toggle("hidden", isDarkMode);
+  document.querySelector("#moon").classList.toggle("hidden", !isDarkMode);
+}
+
+applyInitialTheme();
+
+function toggleTheme() {
+  const isDarkMode = document.documentElement.classList.toggle("dark");
+
+  localStorage.setItem("theme", isDarkMode ? "dark" : "light");
+
+  document.querySelector("#sun").classList.toggle("hidden", isDarkMode);
+  document.querySelector("#moon").classList.toggle("hidden", !isDarkMode);
+}
+
+const themeToggle = document.querySelector("#theme-toggle");
+themeToggle.addEventListener("click", toggleTheme);
